f46a9a08112cef5d208a02d2947f4baa58d07126
Russ Hyde <russ.hyde.data@gmail.com>
2021-01-18 10:49:06 +0000

    use os.getenv() not os.environ.get()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -20,7 +20,7 @@ class ProjectVisibilityTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get("STAGING_SERVER")
+        staging_server = os.getenv("STAGING_SERVER", "")
         if staging_server:
             self.live_server_url = "http://" + staging_server
 
